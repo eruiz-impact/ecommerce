325f76b44cf94d767de40a2b58314c0486b010bd
Erick Ruiz <erick_ruiz23@yahoo.com>
2020-12-17 15:14:30 -0600

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '10ca7da03100ec579fc3986b3ee047ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd4eaeef9-3c63-4b8d-a62f-6891d6052a34'
+};
